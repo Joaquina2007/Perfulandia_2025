13ed28dcae7d109de5dbb676ae7e51efffe1b961
Claudia ubilla letelier <Joquina2007>
2025-05-20 19:54:03 -0400

    Mensaje de tu commit

---
+++ src/main/java/com/Perfulandia_2025/Perfulandia_2025/Perfulandia2025Application.java
@@ -0,0 +1,13 @@
+package com.Perfulandia_2025.Perfulandia_2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Perfulandia2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Perfulandia2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/Perfulandia_2025/Perfulandia_2025/Perfulandia2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Perfulandia_2025.Perfulandia_2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Perfulandia2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
