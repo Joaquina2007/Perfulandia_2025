7f8b1c4c38678cebd1b4eab8c3232e36ca29f22f
Claudia ubilla letelier <Joquina2007>
2025-05-24 10:58:44 -0400

    Cristopher Repositorio actualizado

--- src/main/java/com/Perfulandia_2025/Perfulandia_2025/repository/ClienteRepository.java
+++ src/main/java/com/Perfulandia_2025/Perfulandia_2025/repository/ClienteRepository.java
@@ -1,15 +1,15 @@
 package com.Perfulandia_2025.Perfulandia_2025.repository;
 
 import com.Perfulandia_2025.Perfulandia_2025.modelo.ClienteModel;
-import com.Perfulandia_2025.Perfulandia_2025.responseDTO.ClienteResponseDTO;
+import com.Perfulandia_2025.Perfulandia_2025.responseDTO.ClienteResponseDTO; // Este es tu import actual
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-interface ClienteRepository extends JpaRepository<ClienteModel, Long> {
+public interface ClienteRepository extends JpaRepository<ClienteModel, Long> {
 
-    @Query("select new com.Perfulandia_2025.Perfulandia_2025.dto.response.ClienteResponseDTO(c) " +
+    @Query("select new com.Perfulandia_2025.Perfulandia_2025.responseDTO.ClienteResponseDTO(c) " + // ¡Cambiado aquí!
             "from ClienteModel c where c.activo = true")
     List<ClienteResponseDTO> findClientesActivos();
 }
