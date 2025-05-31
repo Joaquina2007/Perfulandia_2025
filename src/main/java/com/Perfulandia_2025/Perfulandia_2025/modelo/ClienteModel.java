d81a8af0d791c267b870deda02287ff015b9b717
XxCristopherxXC <cr.barrueto@duocuc.cl>
2025-05-30 22:17:47 -0400

    new model

--- src/main/java/com/Perfulandia_2025/Perfulandia_2025/modelo/ClienteModel.java
+++ src/main/java/com/Perfulandia_2025/Perfulandia_2025/modelo/ClienteModel.java
@@ -1,55 +1,33 @@
 package com.Perfulandia_2025.Perfulandia_2025.modelo;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "Clientes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ClienteModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
+    @Column(nullable = false)
     private String nombre;
 
+    @Column(nullable = false)
     private String correo;
 
-    private boolean activo;
+    @Column(nullable = false)
+    private String activo;
 
-    public ClienteModel() {
-        this.activo = true;
-    }
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getCorreo() {
-        return correo;
-    }
-
-    public void setCorreo(String correo) {
-        this.correo = correo;
-    }
-
-    public boolean isActivo() {
-        return activo;
-    }
-
-    public void setActivo(boolean activo) {
-        this.activo = activo;
-    }
 }
