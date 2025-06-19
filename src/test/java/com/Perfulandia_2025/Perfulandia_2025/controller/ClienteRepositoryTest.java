e33253faacc5bfd1cd32c9ea25d7fb1b44d090cf
XxCristopherxXC <cr.barrueto@duocuc.cl>
2025-06-19 02:30:21 -0400

    Add files via upload

--- src/test/java/com/Perfulandia_2025/Perfulandia_2025/controller/ClienteRepositoryTest.java
+++ src/test/java/com/Perfulandia_2025/Perfulandia_2025/controller/ClienteRepositoryTest.java
@@ -1,4 +1,44 @@
 package com.Perfulandia_2025.Perfulandia_2025.controller;
 
+import com.Perfulandia_2025.Perfulandia_2025.modelo.ClienteModel;
+import com.Perfulandia_2025.Perfulandia_2025.repository.ClienteRepository;
+import com.Perfulandia_2025.Perfulandia_2025.responseDTO.ClienteResponseDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 public class ClienteRepositoryTest {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    @Test
+    public void testObtenerTodosClientesDTO() {
+        ClienteModel cliente1 = new ClienteModel();
+        cliente1.setNombre("Cristopher");
+        cliente1.setCorreo("cristopher@example.com");
+        clienteRepository.save(cliente1);
+
+        ClienteModel cliente2 = new ClienteModel();
+        cliente2.setNombre("Constanza");
+        cliente2.setCorreo("constanza@example.com");
+        clienteRepository.save(cliente2);
+
+
+        List<ClienteResponseDTO> clientesDTO = clienteRepository.obtenerTodosClientesDTO();
+
+
+        assertThat(clientesDTO).isNotNull();
+        assertThat(clientesDTO.size()).isEqualTo(2);
+
+
+        assertThat(clientesDTO)
+                .extracting(ClienteResponseDTO::getNombre)
+                .containsExactlyInAnyOrder("Cristopher", "Constanza");
+    }
 }
