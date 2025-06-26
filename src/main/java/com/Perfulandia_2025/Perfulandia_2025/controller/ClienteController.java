9026bd5ac66a5bb78d224a6418d7912c285ead75
XxCristopherxXC <cr.barrueto@duocuc.cl>
2025-06-25 21:57:03 -0400

    Add files via upload

--- src/main/java/com/Perfulandia_2025/Perfulandia_2025/controller/ClienteController.java
+++ src/main/java/com/Perfulandia_2025/Perfulandia_2025/controller/ClienteController.java
@@ -5,56 +5,62 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.Perfulandia_2025.Perfulandia_2025.modelo.ClienteModel;
+import com.Perfulandia_2025.Perfulandia_2025.requestDTO.ClienteRequestDTO;
 import com.Perfulandia_2025.Perfulandia_2025.service.ClienteService;
 
 import java.util.List;
 
-//http://localhost:8080/api/v1/inventario
 @RestController
 @RequestMapping("/api/v1/cliente")
 public class ClienteController {
 
     @Autowired
-
     private ClienteService cliserv;
 
     @GetMapping
     public ResponseEntity<List<ClienteModel>> listar() {
         List<ClienteModel> cliList = cliserv.findAll();
         if(cliList.isEmpty()) {
-            return  ResponseEntity.noContent().build();
-        }return ResponseEntity.ok(cliList);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(cliList);
     }
 
     @PostMapping
-    public ResponseEntity<ClienteModel> guardar(@RequestBody ClienteModel clisave) {
-        ClienteModel newCliente = cliserv.save(clisave);
+    public ResponseEntity<ClienteModel> guardar(@RequestBody ClienteRequestDTO clisave) {
+        ClienteModel newCliente = new ClienteModel();
+        newCliente.setNombre(clisave.getNombre());
+        newCliente.setCorreo(clisave.getCorreo());
+        newCliente.setActivo(clisave.getActivo());
+        newCliente = cliserv.save(newCliente);
         return ResponseEntity.status(HttpStatus.CREATED).body(newCliente);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ClienteModel> actualizar(@PathVariable Integer id,@RequestBody ClienteModel cliDelete) {
+    public ResponseEntity<ClienteModel> actualizar(@PathVariable Integer id, @RequestBody ClienteRequestDTO cliUpdate) {
         try {
             ClienteModel cli = cliserv.findById(id);
-            cli.setId(id);
-            cli.setNombre(cliDelete.getNombre());
-            cli.setCorreo(cliDelete.getCorreo());
-            cli.setActivo(cliDelete.getActivo());
+            if (cli == null) {
+                return ResponseEntity.notFound().build();
+            }
+            cli.setNombre(cliUpdate.getNombre());
+            cli.setCorreo(cliUpdate.getCorreo());
+            cli.setActivo(cliUpdate.getActivo());
             cliserv.save(cli);
-            return ResponseEntity.ok(cliDelete);
+            return ResponseEntity.ok(cli);
         } catch (Exception e) {
             return ResponseEntity.notFound().build();
         }
-
     }
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> eliminar(@PathVariable Long id) {
-        try{
+    public ResponseEntity<?> eliminar(@PathVariable Integer id) {
+        try {
             cliserv.delete(id);
-            return  ResponseEntity.noContent().build();
-        } catch (Exception e){
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
             return ResponseEntity.notFound().build();
         }
     }
-
 }
+
