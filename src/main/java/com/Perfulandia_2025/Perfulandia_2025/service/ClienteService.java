1bf0be91a90bba872c450fdeb5a643143cb274ea
XxCristopherxXC <cr.barrueto@duocuc.cl>
2025-06-25 22:03:02 -0400

    Add files via upload

--- src/main/java/com/Perfulandia_2025/Perfulandia_2025/service/ClienteService.java
+++ src/main/java/com/Perfulandia_2025/Perfulandia_2025/service/ClienteService.java
@@ -7,6 +7,7 @@ import com.Perfulandia_2025.Perfulandia_2025.modelo.ClienteModel;
 import com.Perfulandia_2025.Perfulandia_2025.repository.ClienteRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Transactional
 @Service
@@ -15,25 +16,24 @@ public class ClienteService {
     @Autowired
     private ClienteRepository cliRepository;
 
-    //Metodo Listar
-    public List<ClienteModel> findAll(){
+    // Metodo Listar
+    public List<ClienteModel> findAll() {
         return cliRepository.findAll();
-
     }
-    //Metodo Guardar
-    public ClienteModel save(ClienteModel climod){
+
+    // Metodo Guardar
+    public ClienteModel save(ClienteModel climod) {
         return cliRepository.save(climod);
     }
-    //Metodo Eliminar
-    public void delete(long id){
+
+    // Metodo Eliminar
+    public void delete(Integer id) {
         cliRepository.deleteById(id);
     }
-    //Metodo para Actualizar
-    public ClienteModel findById(long id){
-        return cliRepository.findById(id).get();
+
+    // Metodo para Actualizar
+    public ClienteModel findById(Integer id) {
+        Optional<ClienteModel> cliente = cliRepository.findById(id);
+        return cliente.orElse(null);
     }
-
-
-
-
 }
