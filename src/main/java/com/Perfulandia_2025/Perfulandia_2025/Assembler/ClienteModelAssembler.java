8b423f94cf8848d3c86235e03b3e7c6095207fcb
XxCristopherxXC <cr.barrueto@duocuc.cl>
2025-06-27 00:42:57 -0400

    Add files via upload

--- src/main/java/com/Perfulandia_2025/Perfulandia_2025/Assembler/ClienteModelAssembler.java
+++ src/main/java/com/Perfulandia_2025/Perfulandia_2025/Assembler/ClienteModelAssembler.java
@@ -1,4 +1,23 @@
 package com.Perfulandia_2025.Perfulandia_2025.Assembler;
 
-public class ClienteModelAssembler {
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import com.Perfulandia_2025.Perfulandia_2025.controller.ClienteController;
+import com.Perfulandia_2025.Perfulandia_2025.modelo.ClienteModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClienteModelAssembler implements RepresentationModelAssembler<ClienteModel, EntityModel<ClienteModel>> {
+
+    @Override
+    public EntityModel<ClienteModel> toModel(ClienteModel cliente) {
+        return EntityModel.of(cliente,
+                linkTo(methodOn(ClienteController.class).listar()).withRel("clientes"),
+                linkTo(methodOn(ClienteController.class).actualizar(cliente.getId(), null)).withSelfRel()
+        );
+    }
 }
+
