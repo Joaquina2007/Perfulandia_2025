f8c7f6e129a3943e755cd36bc7b4259790eeebb0
XxCristopherxXC <cr.barrueto@duocuc.cl>
2025-06-27 00:54:19 -0400

    Update ItemPedidoAssembler.java

--- src/main/java/com/Perfulandia_2025/Perfulandia_2025/Assembler/ItemPedidoAssembler.java
+++ src/main/java/com/Perfulandia_2025/Perfulandia_2025/Assembler/ItemPedidoAssembler.java
@@ -1,4 +1,20 @@
 package com.Perfulandia_2025.Perfulandia_2025.Assembler;
 
-public class ItemPedidoAssembler {
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import com.Perfulandia_2025.Perfulandia_2025.controller.ItemPedidoController;
+import com.Perfulandia_2025.Perfulandia_2025.model.ItemPedido;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ItemPedidoAssembler implements RepresentationModelAssembler<ItemPedido, EntityModel<ItemPedido>> {
+
+    @Override
+    public EntityModel<ItemPedido> toModel(ItemPedido itemPedido) {
+        return EntityModel.of(itemPedido,
+                linkTo(methodOn(ItemPedidoController.class).getItemPedidoById(itemPedido.getId())).withSelfRel(),
+                linkTo(methodOn(ItemPedidoController.class).getAllItemsPedido()).withRel("itemsPedidos")); // Nombre del método confirmado
+    }
 }
