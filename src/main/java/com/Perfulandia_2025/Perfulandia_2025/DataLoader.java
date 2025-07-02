259fe5175492daeec661b73a5d8f51de3b37e4c4
Joaquina2007 <cl.ubilla@duocuc.cl>
2025-07-01 21:01:46 -0400

    Add files via upload

--- src/main/java/com/Perfulandia_2025/Perfulandia_2025/DataLoader.java
+++ src/main/java/com/Perfulandia_2025/Perfulandia_2025/DataLoader.java
@@ -18,6 +18,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Random;
 
 @Component
@@ -53,26 +54,28 @@ public class DataLoader implements CommandLineRunner {
         }
         System.out.println("Clientes cargados.");
 
-        for (int i = 0; i < 50; i++) {
-            ItemPedido itemPedido = new ItemPedido();
-            itemPedido.setId((long) (i + 1));
-            itemPedido.setCodigoProducto(faker.code().asin());
-            itemPedido.setDescripcion(faker.educator().course());
-            itemPedido.setPrecioUnitario(new BigDecimal(faker.commerce().price()));
-            itemPedidoRepository.save(itemPedido);
-        }
-
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 10; i++) {
             PedidoReabastecimiento pedidoReabastecimiento = new PedidoReabastecimiento();
-            pedidoReabastecimiento.setId((long) (i + 1));
+            //pedidoReabastecimiento.setId((long) (i + 1));
             pedidoReabastecimiento.setEstadoPedido(faker.educator().course());
-            pedidoReabastecimiento.setTotalPedido(Integer.valueOf(faker.commerce().price()));
+            pedidoReabastecimiento.setTotalPedido(faker.number().numberBetween(100, 50000));
+
+            int numItems = random.nextInt(3) + 1;
+            for (int j = 0; j < numItems; j++) {
+                ItemPedido itemPedido = new ItemPedido();
+                //itemPedido.setId((long) (i + 1));
+                itemPedido.setCodigoProducto(faker.code().asin());
+                itemPedido.setDescripcion(faker.educator().course());
+                itemPedido.setPrecioUnitario(new BigDecimal(faker.commerce().price()));
+                pedidoReabastecimiento.addItemPedido(itemPedido);
+            }
             pedidoReabastecimientoRepository.save(pedidoReabastecimiento);
+
         }
 
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 10; i++) {
             Proveedor proveedor = new Proveedor();
-            proveedor.setId((long) (i + 1));
+            //proveedor.setId((long) (i + 1));
             proveedor.setEstadoProveedor(random.nextBoolean());
             proveedor.setIdentificacion(faker.educator().course());
             proveedor.setNombre(faker.name().fullName());
@@ -82,9 +85,9 @@ public class DataLoader implements CommandLineRunner {
             proveedorRepository.save(proveedor);
         }
 
-        for (int i = 0; i < 50; i++) {
+        for (int i = 0; i < 10; i++) {
             RecepcionMercancia recepcionMercancia = new RecepcionMercancia();
-            recepcionMercancia.setId((long) (i + 1));
+            //recepcionMercancia.setId((long) (i + 1));
             recepcionMercancia.setEstadoRecepcion(faker.educator().course());
             recepcionMercancia.setNotasRecepcion(faker.educator().course());
             recepcionMercancia.setFechaRecepcion(LocalDate.now());
